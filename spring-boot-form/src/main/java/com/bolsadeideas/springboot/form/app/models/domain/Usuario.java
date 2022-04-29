4ce6a778c28a4b1e17677a07913213a976a27bc4
Johnatan <joasalazargi@outlook.com>
2022-04-29 12:40:50 -0500

    Se agrega más clases.

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -3,11 +3,13 @@ package com.bolsadeideas.springboot.form.app.models.domain;
 import java.util.Date;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Future;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 //import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -44,7 +46,8 @@ public class Usuario {
 	private Integer cuenta;
 	
 	@NotNull
-	@DateTimeFormat(pattern="yyyy/MM/dd")
+	@Future
+	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date fechaNacimiento;
 
 	public Usuario() {
